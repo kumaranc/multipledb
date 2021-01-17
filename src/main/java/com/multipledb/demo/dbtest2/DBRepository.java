a2e9e2a728e4343bed8afaf67c2ea71ca9ce6713
kumaranc <c.kumaran.kumaran@gmail.com>
2021-01-17 20:38:36 +0530

    Add files via upload

--- src/main/java/com/multipledb/demo/dbtest2/DBRepository.java
+++ src/main/java/com/multipledb/demo/dbtest2/DBRepository.java
@@ -1,8 +1,9 @@
 package com.multipledb.demo.dbtest2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DBRepository extends JpaRepository<DataTest, Integer> {
+public interface DBRepository extends JpaRepository<DataTest, Integer>, JpaSpecificationExecutor<DataTest> {
 }
